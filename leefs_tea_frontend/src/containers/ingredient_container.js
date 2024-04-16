0c0fd4f61b61e15e1b70acaa14da6bb7f99c32ca
Alethia Quintero <alethiaq22@gmail.com>
2024-04-16 18:25:38 -0400

    create mock ingredient json

--- leefs_tea_frontend/src/containers/ingredient_container.js
+++ leefs_tea_frontend/src/containers/ingredient_container.js
@@ -1,9 +1,10 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Card, CardHeader, CardBody, Heading, Stack, StackDivider, Text } from '@chakra-ui/react'
+import * as list from '../mock_ingredients.json'
 const IngredientContainer = () => {
 
     return (
-        <Box className="logo-text-bold-italic"  fontSize={'56px'} p={10}>
-          
+        <Box className="">
+    
         </Box>
     )
 }
