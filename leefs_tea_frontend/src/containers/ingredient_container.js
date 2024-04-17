5b1b7ab2c96163bb7cd4f6119fabb73d13eb4655
Alethia Quintero <alethiaq22@gmail.com>
2024-04-17 18:40:16 -0400

    update json import

--- leefs_tea_frontend/src/containers/ingredient_container.js
+++ leefs_tea_frontend/src/containers/ingredient_container.js
@@ -1,10 +1,44 @@
 import { Box, Card, CardHeader, CardBody, Heading, Stack, StackDivider, Text } from '@chakra-ui/react'
-import * as list from '../mock_ingredients.json'
+import list from '../mock_ingredients.json'
 const IngredientContainer = () => {
-
+    const items = JSON.parse(JSON.stringify(list));
+    console.log(items);
     return (
         <Box className="">
-    
+            <Card>
+                <CardHeader>
+                    <Heading size='md'>Ingredients</Heading>
+                </CardHeader>
+
+                <CardBody>
+                    <Stack divider={<StackDivider />} spacing='4'>
+                        <Box>
+                            <Heading size='xs' textTransform='uppercase'>
+                                Summary
+                            </Heading>
+                            <Text pt='2' fontSize='sm'>
+                                View a summary of all your clients over the last month.
+                            </Text>
+                        </Box>
+                        <Box>
+                            <Heading size='xs' textTransform='uppercase'>
+                                Overview
+                            </Heading>
+                            <Text pt='2' fontSize='sm'>
+                                Check out the overview of your clients.
+                            </Text>
+                        </Box>
+                        <Box>
+                            <Heading size='xs' textTransform='uppercase'>
+                                Analysis
+                            </Heading>
+                            <Text pt='2' fontSize='sm'>
+                                See a detailed analysis of all your business clients.
+                            </Text>
+                        </Box>
+                    </Stack>
+                </CardBody>
+            </Card>
         </Box>
     )
 }
